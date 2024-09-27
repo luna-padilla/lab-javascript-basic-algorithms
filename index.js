94002521294b187ef10c25d22a98131f05b8a34e
luna <lunapadilla.8@gmail.com>
2024-09-27 18:50:42 +0200

    finished exercise 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+hacker1 = "driver";
+console.log(`The driver's name is ${hacker1}`);
+hacker2 = "navigator1";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
