0caf818da7b69bd1e97757f682ae48fa9ad2aef0
luna <lunapadilla.8@gmail.com>
2024-09-27 19:16:09 +0200

    finished exercise 3.2

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
-    console.log(
-      `The driver has the longest name, it has ${hacker1.length} characters`
-    );
-  } else if (hacker2.length > hacker1.length) {
-    console.log(
-      `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
-    );
-  } else if (hacker1.length === hacker2.length) {
-    console.log(`Wow, you both have equally long names, ${hacker1} characters!.`);
-  }
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters`
+  );
+} else if (hacker2.length > hacker1.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else if (hacker1.length === hacker2.length) {
+  console.log(`Wow, you both have equally long names, ${hacker1} characters!.`);
+}
 
 // Iteration 3: Loops
+
+// 3.1 Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
 let char = "";
 for (let i = 0; i < hacker1.length; i++) {
   if (i === hacker1.length - 1) {
@@ -27,3 +29,14 @@ for (let i = 0; i < hacker1.length; i++) {
   }
 }
 console.log(`${char} `);
+// 3.2 Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+
+function printWordBackwards(word) {
+  let backwardsWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    backwardsWord += word[i];
+  }
+  return backwardsWord;
+}
+const reverseWord = printWordBackwards("Jhon");
+console.log(reverseWord);
