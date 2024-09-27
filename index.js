f9c95eb2ef9ae099eb6ca8d9f5b812fb453ee5e8
luna <lunapadilla.8@gmail.com>
2024-09-27 23:33:43 +0200

    exercise finish 3.3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
 hacker1 = "driver";
 console.log(`The driver's name is ${hacker1}`);
-hacker2 = "navigator1";
+hacker2 = "navigator";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
@@ -40,3 +40,52 @@ function printWordBackwards(word) {
 }
 const reverseWord = printWordBackwards("Jhon");
 console.log(reverseWord);
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+
+// The driver's name goes first.
+// Yo, the navigator goes first, definitely.
+// ` What?! You both have the same name?.`
+
+function getLexicographicOrder(hacker1, hacker2) {
+  const driversMessage = `The driver's name goes first.`;
+  const navigatorsMessage = `Yo, the navigator goes first, definitely.`;
+  const equalsMessage = `What?! You both have the same name?.`;
+
+  // Busco la cadena más pequeña para recorrer como
+  // máximo ese número de caracteres en ambas y no excederme
+  // de las posiciones del array que sea menor
+  let smallerString = hacker1.length;
+  if (hacker1.length < hacker2.length) {
+    smallerString = hacker1.length;
+  } else if (hacker2.length < hacker1.length) {
+    smallerString = hacker2.length;
+  }
+
+  // Busco el primer caracter ASCII que es menor
+  // y en el primero que coincida retorno el mensaje
+  // correspondiente
+  for (let i = 0; i < smallerString; i++) {
+    if (hacker1[i] < hacker2[i]) {
+      return driversMessage;
+    } else if (hacker2[i] < hacker1[i]) {
+      return navigatorsMessage;
+    }
+  }
+
+  // Si sale del bucle, la cadena más corta tiene
+  // identicos caracteres que el inicio de la más larga o
+  // bien son iguales.
+  // En tal caso irá antes el mensaje de la mas corta
+  // o bien el mensaje de que son iguales
+
+  if (hacker1.length < hacker2.length) {
+    return driversMessage;
+  } else if (hacker1.length > hacker2.length) {
+    return navigatorsMessage;
+  } else {
+    return equalsMessage;
+  }
+}
+const msg = getLexicographicOrder(hacker1, hacker2);
+console.log(msg);
